01d58a47e6e3c235cc92a03f9496a6b16ccba092
RyanKaleliGabriel <ryang9241@gmail.com>
2023-07-05 16:58:00 +0300

    NavBar

---
+++ src/components/Home.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import promoting from "../assests/promotingimg.png"
+import enganging from "../assests/engagingimg.png";
+import influencing from "../assests/influencingimg.png";
+import growing from "../assests/growingimg.png";
+
+function Home() {
+  return (
+    <div>
+      <div className="homeContainer">
+        <div className="homeItems">
+        <h1>It's all about <span>Growing</span> your social media.</h1>
+        </div>
+        <div className="homeItems">
+        <img src="" className="home-imgs"></img>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -33,30 +33,38 @@ function NavBar(){
     function handleMouseOut(){
         setHover(false)
     }
-    function handleClick(){
-        setMenu(true)
+    function toggleMenu(){
+        setMenu(!isMenu)
     }
 
     
 
     return(
+        <div>
         <div className="navContainer">
             <nav className="nav">
             <img  className="resurface-logo" src={logo} alt="#"></img>
+            <section id="navSection">
                 <a href="/#" className="navItems border-items">HOME</a>
                 {links.map(createNavLink)}
-                <a href="/#" className="navItems" id="download-btn">DOWNLOAD</a>
-                <button id="navMenu" onClick={handleClick}>
+                <a href="/#" className="navItems " id="download-btn">DOWNLOAD</a>
+            </section>    
+                <button id="navMenu" onClick={toggleMenu}>
                 <MenuIcon className="navMenu" />
                 </button>
             </nav>      
-            <div className="menuItems" style={isMenu ? {display:"flex"}:{}}>
-                  <a href="/#" className="navItems border-items">HOME</a>
+        </div>
+        { isMenu && ( 
+            <div className="menuItems" >
+            <a href="/#" className="navItems border-items">HOME</a>
                    {links.map(createNavLink)}
-                 <a href="/#" className="navItems" onMouseOver={handlMouseOver} onMouseOut={handleMouseOut} style={isHover ? {border: "1px solid white"} : {} }>DOWNLOAD</a>
-            </div> 
+            <a href="/#" className="navItems" onMouseOver={handlMouseOver} onMouseOut={handleMouseOut} style={isHover ? {border: "1px solid white"} : {} }>DOWNLOAD</a>
+        </div> 
+
+        )}
+
         </div>
     )
 }
-
+//
 export default NavBar
